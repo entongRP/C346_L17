a96c2589d58f639e5879ceffb0c168c02412548d
entongRP <24012306@myrp.edu.sg>
2026-01-14 15:30:13 +0800

    done

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState, useEffect} from 'react';
+import { FlatList, StatusBar, Text, TextInput, View, Image} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+//1C
+let originalData = [];
+
+const App = () => {
+    const [myData, setMyData] = useState([]);
+
+    //add effect 1B
+    useEffect(() => {
+        //add fetch 1A
+        const myurl = "https://onlinecardappwebservice-09rt.onrender.com/allcards"
+        fetch(myurl)
+            .then ((response)=> {
+                return response.json();
+            })
+            .then( (myJson)=> {
+                setMyData(myJson);
+                originalData = myJson;
+            })
+    }, [])
+
+
+    const FilterData = (text) => {
+        if (text!=''){
+            let myFilterData = originalData.filter((item)=> item.card_name.toLowerCase().includes(text.toLowerCase()));
+            setMyData(myFilterData);
+        } else {
+            setMyData(originalData);
+        }
+    }
+
+    const renderItem = ({item, index}) => {
+        return (
+            <View
+                style={{
+                    borderWidth: 1,
+                    borderRadius: 10,
+                    padding: 10,
+                    marginVertical: 8,
+                    flexDirection: 'row',
+                    alignItems: 'center'
+                }}
+            >
+                <Text style={{fontWeight:'bold'}}>{item.card_name}</Text>
+                <Image
+                    source={{ uri: item.card_pic }}
+                    style={{
+                        width: 100,
+                        height: 150,
+                        borderWidth: 1,
+                        borderRadius: 8,
+                        marginVertical: 6,
+                        marginStart: 40
+                    }}
+                />
+            </View>
+        );
+    };
+
+    return (
+        <View>
+            <StatusBar/>
+            <Text>Search:</Text>
+            <TextInput style={{borderWidth:1}} onChangeText={(text)=> {FilterData(text)}}/>
+            <FlatList data={myData} renderItem={renderItem} />
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
